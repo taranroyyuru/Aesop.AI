5a8c5bec5f6bd76667d4a3c79a20a4bbbce12e47
brad <jeongjooho1995@gmail.com>
2024-10-19 00:43:10 -0700

    for frontend usage

---
+++ aesop/agents.py
@@ -0,0 +1,23 @@
+import os
+import google.generativeai as genai
+
+
+
+def image_agent(prompt_text):
+	genai.configure(api_key=os.environ['API_KEY'])
+	imagen = genai.ImageGenerationModel("imagen-3.0-generate-001")
+
+	result = imagen.generate_images(
+		prompt=prompt_text,
+		number_of_images=1,
+		safety_filter_level="block_only_high",
+		person_generation="allow_adult",
+		aspect_ratio="3:4",
+		negative_prompt="Outside",
+	)
+	
+	return result.images
+  # Open and display the image using your local operating system.
+  		
+
+
