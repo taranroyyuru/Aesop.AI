f0b13dc74b5a21e3e0b72568e528b3b98851e55f
devanshu <devanshusenp@gmail.com>
2024-10-19 08:50:23 -0700

    more login stuff

--- aesop/frontend/pages/story.py
+++ aesop/frontend/pages/story.py
@@ -2,12 +2,23 @@
 
 import reflex as rx
 
+from aesop.frontend.components.footer import index as footer
+from aesop.frontend.components.header import index as header
 from aesop.frontend.utils import BaseState
+
+
+def story() -> rx.Component:
+    return rx.vstack(
+        rx.heading("Story 2"),
+        rx.text("Welcome to the Story page!"),
+    )
 
 
 @rx.page(on_load=BaseState.get_configs)
 def story_page() -> rx.Component:
-    return rx.vstack(
-        rx.heading("Story 2"),
-        rx.text("Welcome to the Story page!"),
+    return rx.container(
+        header(),
+        story(),
+        footer(),
+        padding="0",
     )
